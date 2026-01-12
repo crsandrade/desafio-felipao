b037d448bf6c07c148fb765b3568d9cb58579f9f
Rafael Andrade <rafael_asc@outlook.com>
2026-01-12 13:42:48 -0300

    Novo desafio do felipao realizado

---
+++ desafio2.js
@@ -0,0 +1,23 @@
+const randomWin = Math.random() * (100 - 1) + 1
+
+
+function defineRank(wins) {
+    if (wins <= 10) {
+        return 'Ferro'
+    } else if (wins <= 20) {
+        return 'Bronze'
+    } else if (wins <= 50) {
+        return 'Prata'
+    } else if (wins <= 80) {
+        return 'Ouro'
+    } else if (wins <= 90) {
+        return 'Diamante'
+    } else if (wins <= 100) {
+        return 'Lendário'
+    } else {
+        return 'Imortal'
+    }
+}
+
+
+console.log(`O Herói tem saldo de ${randomWin.toFixed(0)} vitórias e está no nível ${defineRank(randomWin.toFixed(0))}`)
